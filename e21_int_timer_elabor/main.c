c7bd847e8bd85ba38b44871d5250bb1a8902d2ca
olekmali <olekmali@ieee.org>
2017-02-15 13:00:05 -0600

    Added watchdog and sleep to the example E21 - elaborate use of timer for detexcting button sequencing and generating LED sequences.

--- e21_int_timer_elabor/main.c
+++ e21_int_timer_elabor/main.c
@@ -1,9 +1,11 @@
 // Copyright (C) 2013-2017 Aleksander Malinowski
 
-#include "bu_init.h"
-#include "timer3int.h"
 #include "c8051F120.h"                  // Device-specific SFR Definitions
 #include "c8051F120_io.h"               // Device-specific SFR Definitions
+
+#include "bu_init.h"
+#include "bu_watchdog.h"
+#include "timer3int.h"
 
 #include <stdint.h>
 
@@ -16,9 +18,8 @@ void main(void)
     uint16_t  time_SW2_pressed  = 0;
     uint16_t  time_SW2_released = 0;
 
-    // Disable watchdog timer
-    WDTCN = 0xde;
-    WDTCN = 0xad;
+    // Set up watchdog timer (10.4ms at sysclk of 100MHz)
+    WatchDog_set_10ms();
 
     PORT_Init ();
     SYSCLK_Init();
@@ -31,9 +32,19 @@ void main(void)
     while (1)
     {
         // wait for Timer3 interrupt, when it ends
-        while( !Timer3_GetSemaphore() ) ;
+        while( !Timer3_GetSemaphore() ) {
+            // WatchDog_reset();    // optionally reset watchdog timer while waiting
+                                    // for the right interrupt to be completed
+            
+            // sleep until an interrupt happens and then check [again]
+            // if it is the one we are waiting for to be completed
+            CPU_sleep();
+        }
         // and immediately afterwards reset the interrupt done marker for the next time
         Timer3_ResetSemaphore();
+
+        // Reset watchdog timer
+        WatchDog_reset();
 
         // handle timing of switches
         if (!SW2)

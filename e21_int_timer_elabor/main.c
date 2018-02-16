1883dd5fa09cd864d31408a159bd8f2a3aab471f
olekmali <olekmali@ieee.org>
2018-02-16 00:31:14 -0600

    Cleaned up the Elaborate use of timer example (one of E21)

--- e21_int_timer_elabor/main.c
+++ e21_int_timer_elabor/main.c
@@ -7,8 +7,13 @@
 
 #include <stdint.h>
 
-#define INTERRUPT_RATE  50000UL     // Interrupt frequency in Hz
-#define LOOP_RATE         100U      // Loop semaphore frequency in Hz
+// Frequencies in Hz
+#define INTERRUPT_RATE    1000UL    // Interrupt frequency in Hz
+#define LOOP_RATE          100U     // Loop semaphore frequency in Hz (100Hz == 10ms)
+
+// Time delays in milliseconds
+#define T_DEBOUNCE           5U     // button debouncing time in number of loops (5 @ 100Hz ~~ 50ms)
+#define T_LONG_PRESS       100U     // boundary between short and long depress time in number of loops
 
 void main(void)
 {
@@ -52,38 +57,54 @@ void main(void)
         // **********************************************************************
 
         // handle timing of switches
-        if (!SW2)
+        if (!SW2) // push button depressed
         {
-            if (time_SW2_released>10)               // check if pressed after released for at least 100ms
+            // ignore release times shorter than the debounce time
+            if ( time_SW2_released>T_DEBOUNCE )
             {
-                time_SW2_released = 0;
+                // check how long it was released and insert any action 
+                // that needs to be done if the release time was longer than nnn epochs
+            }
+            time_SW2_released = 0;
+
+            // check if button is being pressed for longer than T_LONG_PRESS epochs
+            if ( time_SW2_pressed>T_LONG_PRESS )
+            {
+                // code specific to the particular application
             }
 
+            // increment only if still within the range to prevent roll over
             if (time_SW2_pressed<0xffff)
             {
-                time_SW2_pressed++;                 // increment only if still within the range to prevent roll over
+                time_SW2_pressed++;
             }
         }
-        else
+        else // push button released
         {
-            if (time_SW2_pressed>10)                // check if was pressed for at least 10 loops - 100ms (debouncing)
+            // ignore push times shorter than the debounce time
+            if ( time_SW2_pressed>T_DEBOUNCE )
             {
-                if (time_SW2_pressed>60)            // check if was pressed for more than 60 loops - 600ms
+                // check if released after a long period of time
+                if (time_SW2_pressed>T_LONG_PRESS)
                 {
                     // code specific to the particular application
-                    if (mode==(MAX_MODES-1)) mode=0;
-                    else mode=(MAX_MODES-1);        // we arranged for the very last mode to be the off mode
+
+                    // we arranged for the very last mode to be the off mode
+                    // so toggling between a non-ff mode and the off mode looks like this:
+                    if (mode==(MAX_MODES-1)) mode=0; else mode=(MAX_MODES-1);
                     Timer3_setMode(mode);
                 }
-                else                                // was pressed for not more than 60 loops (but more than debouncing)
+                else // it was pressed for a short period of time but still longer than debounce time
                 {
                     // code specific to the particular application
-                    // advance to the next mode but skip the "off mode"
+
+                    // we arranged for the very last mode to be the off mode
+                    // so advancement tot he next mode looks like this:
                     mode++; if (mode>=(MAX_MODES-1)) mode=0;
                     Timer3_setMode(mode);
                 }
-                time_SW2_pressed = 0;               // reset the pressed value so that we handle pressed only once
             }
+            time_SW2_pressed = 0;               // reset the pressed value so that we handle pressed only once
 
             if (time_SW2_released<0xffff) {
                 time_SW2_released++;                // increment only if still within the range to prevent roll over
